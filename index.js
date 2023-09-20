49afcd4e5e5deaab36308f3192a2922fc9f4175e
bhavesh_chandiwale <bhavesh_chandiwale@persistent.com>
2023-09-20 12:02:15 +0530

    working workflow

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-const port = process.env.PORT || 3030;
+// const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => res.send('Hello World!!! Onexlab updated version 2'))
+// app.get('/', (req, res) => res.send('Hello World!!! Onexlab updated version 2'))
 
-app.listen(port, (err) => {
-    if (err) {
-      console.log('Error::', err);
-    }
-      console.log(`Onexlab app listening on port ${port}`);
-  });
+// app.listen(port, (err) => {
+//     if (err) {
+//       console.log('Error::', err);
+//     }
+//       console.log(`Onexlab app listening on port ${port}`);
+//   });
+      console.log(`Onexlab app listening on port `)
